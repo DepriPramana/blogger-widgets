e3b021e50bb491dbb6660ae8975c2a16325ab707
makingdifferent <gj37765@gmail.com>
2013-01-25 15:03:50 +0530

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -19,3 +19,24 @@ function getIframeUrl()
 
 	return iframe_url;
 }
+function resizeIframe()
+{
+	$("#iframe").height( WindowHeight() - getObjHeight(document.getElementById("toolbar")) );
+}
+
+function WindowHeight()
+{
+	var de = document.documentElement;
+	return self.innerHeight ||
+		(de &amp;amp;amp;&amp;amp;amp; de.clientHeight ) ||
+		document.body.clientHeight;
+}
+
+function getObjHeight(obj)
+{
+	if( obj.offsetWidth )
+	{
+		return obj.offsetHeight;
+	}
+	return obj.clientHeight;
+}
